a276919bab635b12503c99610a57f5f8b7044042
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2023-12-31 06:18:42 -0600

    Update S19.sh

--- S19.sh
+++ S19.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Function to prompt the user with yes/no question
-function prompt_yes_no {
-    while true; do
-        read -p "$1 (yes/no): " yn
-        case $yn in
-            [Yy]* ) return 0;;  # User selected Yes
-            [Nn]* ) return 1;;  # User selected No
-            * ) echo "Please answer yes or no.";;
-        esac
+# Function to display messages and wait for user input
+function prompt_user() {
+    echo "$1"
+    read -p "Press Enter to continue..."
+}
+
+# Function for countdown
+function countdown() {
+    secs=$1
+    while [ $secs -gt 0 ]; do
+        echo -ne "Starting in $secs seconds...\033[0K\r"
+        sleep 1
+        : $((secs--))
     done
 }
 
@@ -35,25 +39,50 @@ pip install -e .
 echo "Running get_model.sh..."
 ./get_model.sh
 
-# Prompt user to create a new coldkey
-if prompt_yes_no "Create a new coldkey?"; then
-    btcli w new_coldkey
-else
-    echo "Skipping coldkey creation."
+# Create a new coldkey
+read -p "Do you want to create a new coldkey? (yes/no): " create_coldkey
+if [ "$create_coldkey" == "yes" ]; then
+    read -p "Do you want the wallet to have a password? (yes/no): " coldkey_password
+    if [ "$coldkey_password" == "yes" ]; then
+        btcli w new_coldkey
+    else
+        btcli w new_coldkey --no_password
+    fi
 fi
 
-# Prompt user to create a new hotkey
-if prompt_yes_no "Create a new hotkey?"; then
+# Create a new hotkey
+read -p "Do you want to create a new hotkey? (yes/no): " create_hotkey
+if [ "$create_hotkey" == "yes" ]; then
     btcli w new_hotkey
 else
-    echo "Skipping hotkey creation."
+    echo "Ending script. No hotkey created."
+    exit 0
 fi
 
-# Prompt user to save seed phrases and list wallets
-if prompt_yes_no "Have you saved the seed phrases for both wallets?"; then
+# Remind user to save seed phrases and then list new wallets
+read -p "Did you save the seed phrases? (yes/no): " save_seed
+if [ "$save_seed" == "yes" ]; then
     btcli w list
 else
-    echo "DO NOT FORGET TO SAVE YOUR SEED PHRASES!"
+    echo "Script completed without listing wallets."
 fi
 
-echo "Script completed."
+# Prompt user to continue with registration script
+read -p "Do you want to continue with the registration script? (yes/no): " register_script
+if [ "$register_script" == "yes" ]; then
+    echo "Note: This registration script could potentially use all funds in the coldkey."
+       
+    echo "Starting the registration script. This requires the coldkey to not have a password."
+    
+	# Countdown before starting registration attempts
+    countdown 5
+	
+    # Registration script
+    while true
+    do
+        btcli s register --netuid 19 --wallet.name default --wallet.hotkey default --subtensor.network finney --no_prompt
+        sleep 60  # Wait for 1 minute
+    done
+else
+    echo "Script completed successfully. It is now time to fund the coldkey and register on S19."
+fi
